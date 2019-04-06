8cb0df7effa2f13657a13004ab92ad132fec794e
Uvneshkumar <uvneshkumar@gmail.com>
2019-04-06 21:46:24 +0530

    Made Necessary Dependency Changes

--- app/src/main/java/in/petsearch/knowthemovies/MoviesListActivity.java
+++ app/src/main/java/in/petsearch/knowthemovies/MoviesListActivity.java
@@ -1,7 +1,8 @@
 package in.petsearch.knowthemovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MoviesListActivity extends AppCompatActivity {
 
